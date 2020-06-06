ece5665766e984d857f935a60d5955e647a1a049
André Augusto Mitkute do Amaral <andre.augusto22@hotmail.com>
2020-06-06 00:59:41 -0300

    first commit

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,174 @@
+import React, { useState, useEffect } from 'react';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, Text, View, ImageBackground, Image, Alert } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
+import axios from 'axios';
+
+
+interface IBGEUFResponse {
+  sigla: string;
+  nome: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
+interface Item {
+  label: string;
+  value: string;
+}
+
+
+const Home = () => {
+  const navigation = useNavigation();
+  const [ufItems, setUfItems] = useState<Item[]>([]);
+  const [cityItems, setCityItems] = useState<Item[]>([])
+
+  const [selectedUf, setSelectedUf] = useState(null);
+  const [selectedCity, setSelectedCity] = useState('');
+
+  function handleNavigateToPoints() {
+    selectedUf && selectedCity ? navigation.navigate('Points', {uf: selectedUf, city: selectedCity}) : Alert.alert('Selecione um estado e uma cidade para prosseguir');    
+  }
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+      const ufInitials = response.data.map(uf => ({ label: uf.nome, value: uf.sigla }));
+      setUfItems(ufInitials);
+    })
+  }, []);
+
+  useEffect(() => {
+    if (!selectedUf) {
+      return;
+    }
+
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+      const cityNames = response.data.map(city => ({label: city.nome, value: city.nome}));
+
+      setCityItems(cityNames);
+    });
+  }, [selectedUf]);
+
+  return (
+    <ImageBackground
+      source={require('../../assets/home-background.png')}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+      </View>
+      <View style={styles.footer}>
+
+        <RNPickerSelect
+          style={styles.select}
+          value={selectedUf}
+          placeholder={{ value: null, label: 'Selecione um estado' }}
+          onValueChange={value => setSelectedUf(value)}
+          items={ufItems}
+        />
+        <RNPickerSelect
+          style={styles.select}
+          disabled={selectedUf ? false : true}
+          placeholder={{ value: null, label: 'Selecione a cidade' }}
+          onValueChange={value => setSelectedCity(value)}
+          value={selectedCity}
+          items={cityItems}
+        ></RNPickerSelect>
+
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#fff" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {
+    borderRadius: 8,
+    fontSize: 20
+  },
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  }
+});
+
+
+export default Home;
